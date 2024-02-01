19323d3ec5623364434f7c76de08a0942e662f13
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-02-01 19:53:16 +0100

    Implement education term feature and update user auth
    Added functionality for the new EducationTerm domain, involving the creation of corresponding controller, service, repository, and request/response models. These changes facilitate the management of education terms in a school context. The user authentication was also updated to find user details by username, which required enhancements in AuthenticationService and AuthenticationController.

--- src/main/java/com/project/schoolmanagment/repository/businnes/EducationTermRepository.java
+++ src/main/java/com/project/schoolmanagment/repository/businnes/EducationTermRepository.java
@@ -1,10 +1,23 @@
 package com.project.schoolmanagment.repository.businnes;
 
 import com.project.schoolmanagment.entity.concretes.businnes.EducationTerm;
+import com.project.schoolmanagment.entity.enums.Term;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface EducationTermRepository extends JpaRepository<EducationTerm,Long> {
+  
+  
+  @Query("SELECT (count (e) > 0) FROM EducationTerm e WHERE e.term=?1 and extract(YEAR FROM e.startDate) = ?2 ")
+  boolean existByTermAndYear(Term term, int year);
+  
+  
+  //find all education terms in a year
+  @Query("SELECT e FROM EducationTerm e WHERE extract(YEAR FROM e.startDate) = ?1 ")
+  List<EducationTerm>findByYear(int year);
 
 }
--- src/main/java/com/project/schoolmanagment/service/businnes/EducationTermService.java
+++ src/main/java/com/project/schoolmanagment/service/businnes/EducationTermService.java
@@ -1,10 +1,86 @@
 package com.project.schoolmanagment.service.businnes;
 
+import com.project.schoolmanagment.entity.concretes.businnes.EducationTerm;
+import com.project.schoolmanagment.exception.BadRequestException;
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
+import com.project.schoolmanagment.payload.mappers.EducationTermMapper;
+import com.project.schoolmanagment.payload.messages.ErrorMessages;
+import com.project.schoolmanagment.payload.messages.SuccessMessages;
+import com.project.schoolmanagment.payload.request.businnes.EducationTermRequest;
+import com.project.schoolmanagment.payload.response.businnes.EducationTermResponse;
+import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
+import com.project.schoolmanagment.repository.businnes.EducationTermRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EducationTermService {
+  
+  private final EducationTermRepository educationTermRepository;
+  
+  private final EducationTermMapper educationTermMapper;
 
+  public ResponseMessage<EducationTermResponse> saveEducationTerm(
+      EducationTermRequest educationTermRequest) {
+    validateEducationTermDates(educationTermRequest);
+    EducationTerm educationTerm = educationTermMapper.mapEducationTermRequestToEducationTerm(educationTermRequest);
+    
+    return ResponseMessage.<EducationTermResponse>builder()
+        .message(SuccessMessages.EDUCATION_TERM_SAVE)
+        .returnBody(educationTermMapper.mapEducationTermToEducationTermResponse(educationTerm))
+        .httpStatus(HttpStatus.CREATED)
+        .build();
+  }
+  
+  private void  validateEducationTermDates(EducationTermRequest educationTermRequest){
+    validateEducationTermsDatesForRequest(educationTermRequest);
+    //only one education term can exist in a year
+    if(educationTermRepository.existByTermAndYear(
+        educationTermRequest.getTerm(),educationTermRequest.getStartDate().getYear())){
+          throw new ResourceNotFoundException(ErrorMessages.EDUCATION_TERM_IS_ALREADY_EXIST_BY_TERM_AND_YEAR_MESSAGE);
+    }
+    
+    //validate not to have any conflict with other education terms
+    if(educationTermRepository.findByYear(educationTermRequest.getStartDate().getYear())
+        .stream()
+        .anyMatch(educationTerm ->
+            (educationTerm.getStartDate().equals(educationTermRequest.getStartDate()) 
+                || (educationTerm.getStartDate().isBefore(educationTermRequest.getStartDate())
+                && educationTerm.getEndDate().isAfter(educationTermRequest.getStartDate())) 
+                || (educationTerm.getStartDate().isBefore(educationTermRequest.getEndDate())
+                && educationTerm.getEndDate().isAfter(educationTermRequest.getEndDate()))
+                || (educationTerm.getStartDate().isAfter(educationTermRequest.getStartDate()) 
+                && educationTerm.getEndDate().isBefore(educationTermRequest.getEndDate()))))) {
+      throw new BadRequestException(ErrorMessages.EDUCATION_TERM_CONFLICT_MESSAGE);
+    }
+    
+    //TODO try to write a query for this algorithm
+    
+    // 1.start - 1.stop
+    // 2.start - 2.stop
+    
+    // first case 1. education term is earlier than 2 .--> 1.stop  > 2.start
+    // second case  2. education term is earlier than 1. --> 2.stop > 1.start
+    
+    //you should find which one is starting first (stop time) then compare it with second (start time)
+    
+  }
+  
+  private void validateEducationTermsDatesForRequest(EducationTermRequest educationTermRequest){
+    //registration > startDate
+    if(educationTermRequest.getLastRegistrationDate().isAfter(educationTermRequest.getStartDate())){
+      throw new ResourceNotFoundException(
+          ErrorMessages.EDUCATION_START_DATE_IS_EARLIER_THAN_LAST_REGISTRATION_DATE
+      );
+    }    
+    // endDate > startDate
+    if(educationTermRequest.getEndDate().isBefore(educationTermRequest.getStartDate())){
+      throw new ResourceNotFoundException(ErrorMessages.EDUCATION_END_DATE_IS_EARLIER_THAN_START_DATE);
+    }
+  }
+  
+  
+  
 }
